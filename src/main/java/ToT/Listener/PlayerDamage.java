6bef12a3021b59125dd750d6a15ea51fb02fd0a6
Derpy666 <noamgl2002@gmail.com>
2023-07-17 15:22:55 +0300

    make hp at auction bar updated

--- src/main/java/ToT/Listener/PlayerDamage.java
+++ src/main/java/ToT/Listener/PlayerDamage.java
@@ -1,7 +1,8 @@
 package ToT.Listener;
 
 import ToT.Objects.TPlayer;
-import ToT.Utils.PartyManagment;
+import ToT.System.PartyManagment.Utils;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -9,26 +10,45 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
+import static ToT.Utils.Utils.getPlayerData;
+
 public class PlayerDamage implements Listener {
 
     @EventHandler
     public void onEntityDamage(EntityDamageEvent event) {
         EntityDamageEvent.DamageCause murder = event.getCause();
-        Player player = (Player) event.getEntity();
+        Entity entity = event.getEntity();
         int damage = (int) event.getDamage();
 
-        event.setCancelled(true);
+        if(!(entity instanceof Player player)) return;
 
-        TPlayer data = PartyManagment.getData(player.getUniqueId());
+        TPlayer data = getPlayerData(player.getUniqueId());
         int[] stats = data.getStats();
         int[] statPoints = data.getStatPoints();
 
-        System.out.println(damage);
-        int hp = stats[3] - 1;
+        int hp = stats[3] - damage;
         if(hp < 0) hp = 0;
-        System.out.println(hp);
+
+        event.setDamage(damage);
 
         statPoints[3] = hp;
+
+        ArrayList<UUID> party = Utils.getParty(player.getUniqueId());
+
+        if(!Utils.inParty(party, player.getUniqueId())) {
+            return;
+        }
+
+        ArrayList<UUID> members = Utils.getMembers(party);
+
+        members.forEach(uuid -> {
+            Player p = Bukkit.getServer().getPlayer(uuid);
+            if(p != null) Utils.updatePartyScoreboard(p);
+        });
+
     }
 
     @EventHandler
